9ef772b784cb71a0280f915405bcaa8378512a84
Julien GUTTER <julien.gutter@gmail.com>
2025-01-03 17:45:38 +0200

    Enable Feign

--- microservice-clientui/src/main/java/com/openclassrooms/p9/clientUI/ClientUiApplication.java
+++ microservice-clientui/src/main/java/com/openclassrooms/p9/clientUI/ClientUiApplication.java
@@ -2,8 +2,10 @@ package com.openclassrooms.p9.clientUI;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients("com.openclassrooms.p9.clientUI")
 public class ClientUiApplication {
 
 	public static void main(String[] args) {
