46d21bd0cd460f73ec8e78bee77bc0cd15ee74c8
Julien GUTTER <julien.gutter@gmail.com>
2025-01-03 14:03:29 +0200

    Basic configuration of microservice client UI

---
+++ microservice-clientui/src/main/java/com/openclassrooms/p9/clientUI/ClientUiApplication.java
@@ -0,0 +1,13 @@
+package com.openclassrooms.p9.clientUI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientUiApplication.class, args);
+	}
+
+}
