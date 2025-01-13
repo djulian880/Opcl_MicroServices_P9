fe6b3ba503a8715a4a6a1de49521682fc64b391e
Julien GUTTER <julien.gutter@gmail.com>
2025-01-13 11:12:03 +0200

    Microservice notes initial package

---
+++ microservice-notes/src/main/java/com/openclassrooms/p9/microservice_notes/MicroserviceNotesApplication.java
@@ -0,0 +1,13 @@
+package com.openclassrooms.p9.microservice_notes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceNotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceNotesApplication.class, args);
+	}
+
+}
