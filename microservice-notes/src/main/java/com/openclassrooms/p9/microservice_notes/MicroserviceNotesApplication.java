00723ed79d26e4b3e8248de584b3296ffebb722c
Julien GUTTER <julien.gutter@gmail.com>
2025-01-20 14:27:03 +0200

    Ajout client Eureka

--- microservice-notes/src/main/java/com/openclassrooms/p9/microservice_notes/MicroserviceNotesApplication.java
+++ microservice-notes/src/main/java/com/openclassrooms/p9/microservice_notes/MicroserviceNotesApplication.java
@@ -2,6 +2,9 @@ package com.openclassrooms.p9.microservice_notes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
 
 @SpringBootApplication
 public class MicroserviceNotesApplication {
