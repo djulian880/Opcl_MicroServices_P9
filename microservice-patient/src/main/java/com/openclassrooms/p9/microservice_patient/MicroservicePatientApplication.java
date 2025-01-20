d689be95fa52b847c9ec181a9191c25477013e65
Julien GUTTER <julien.gutter@gmail.com>
2025-01-20 14:27:06 +0200

    Ajout client Eureka

--- microservice-patient/src/main/java/com/openclassrooms/p9/microservice_patient/MicroservicePatientApplication.java
+++ microservice-patient/src/main/java/com/openclassrooms/p9/microservice_patient/MicroservicePatientApplication.java
@@ -2,7 +2,10 @@ package com.openclassrooms.p9.microservice_patient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Configuration;
+
+@EnableDiscoveryClient
 
 @SpringBootApplication
 public class MicroservicePatientApplication {
