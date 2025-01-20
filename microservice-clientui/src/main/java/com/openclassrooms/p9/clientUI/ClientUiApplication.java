fa63a0a33c86dcff9414b27a4debab5e626b9bce
Julien GUTTER <julien.gutter@gmail.com>
2025-01-20 14:26:48 +0200

    Ajout client Eureka

--- microservice-clientui/src/main/java/com/openclassrooms/p9/clientUI/ClientUiApplication.java
+++ microservice-clientui/src/main/java/com/openclassrooms/p9/clientUI/ClientUiApplication.java
@@ -3,8 +3,10 @@ package com.openclassrooms.p9.clientUI;
 import com.openclassrooms.p9.clientUI.beans.PatientBean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 @EnableFeignClients("com.openclassrooms.p9.clientUI")
 public class ClientUiApplication {
